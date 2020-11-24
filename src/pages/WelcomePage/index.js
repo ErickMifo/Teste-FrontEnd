cbe0eb3c935023373adcea834b87e1994752547a
Erick Yudi Fukuda Mifo <erickyudi99@gmail.com>
2020-11-23 22:53:51 -0300

    ADD
    +welcome page

--- src/pages/WelcomePage/index.js
+++ src/pages/WelcomePage/index.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import './styles.css';
 import queryString from 'query-string';
+import NavBar from '../../components/NavBar'
+import { Link } from 'react-router-dom'
 
 function WelcomePage({ location }) {
+
     const [name, setName] = useState('');
 
     useEffect(() => {
@@ -14,9 +17,26 @@ function WelcomePage({ location }) {
       }, [location.search]);
       
 return (
+    <>
+        <NavBar />
         <div className="WelcomeContainer">
-           <h1>welcome, {name}</h1> 
+               <h1>welcome, <span style={{color: "#1167b1"}}>{name}</span></h1>
+            <div>
+               <p className="interestsTitle">MY INTERESTS</p>
+                <div className="interests">
+                    <h3 className="politicsInterests">POLITICS</h3>
+                    <h3 className="businessInterests">BUSINESS</h3>
+                    <h3 className="techInterests">TECH</h3>
+                    <h3 className="scienceInterests">SCIENCE</h3>
+                    <h3 className="sportsInterests">SPORTS</h3>
+                </div>
+            </div>
+            <button className="buttonSave"> SAVE </button>
+            <Link to='/'>
+            <button className="buttonBackHome"> BACK TO HOME </button>
+            </Link>
         </div>
+    </>
     )
 }
 
