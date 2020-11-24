f4a9d7253f3f02c9570aec9b37307dfddb66443e
Erick Yudi Fukuda Mifo <erickyudi99@gmail.com>
2020-11-24 17:17:02 -0300

    ADD
    +onClick function to interests

--- src/pages/WelcomePage/index.js
+++ src/pages/WelcomePage/index.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from "react";
 import './styles.css';
 import queryString from 'query-string';
-import NavBar from '../../components/NavBar'
-import { Link } from 'react-router-dom'
+import NavBar from '../../components/NavBar';
+import { Link } from 'react-router-dom';
 
 function WelcomePage({ location }) {
 
     const [name, setName] = useState('');
+    const [politics, setPolitics] = useState(false);
+    const [business, setBusiness] = useState(false);
+    const [tech, setTech] = useState(false);
+    const [science, setScience] = useState(false);
+    const [sports, setSports] = useState(false);
+
 
     useEffect(() => {
 
@@ -15,20 +21,41 @@ function WelcomePage({ location }) {
         setName(name)
        
       }, [location.search]);
+
+      const handleClickPolitics = (e) => {
+        console.log(e.target.className)
+        setPolitics(!politics)
+      }
+      const handleClickBusiness = (e) => {
+        console.log(e.target.className)
+        setBusiness(!business)
+      }
+      const handleClickTech = (e) => {
+        console.log(e.target.className)
+        setTech(!tech)
+      }
+      const handleClickScience = (e) => {
+        console.log(e.target.className)
+        setScience(!science)
+      }
+      const handleClickSports = (e) => {
+        console.log(e.target.className)
+        setSports(!sports)
+      }
       
 return (
     <>
         <NavBar />
         <div className="WelcomeContainer">
-               <h1>welcome, <span style={{color: "#1167b1"}}>{name}</span></h1>
-            <div>
+               <h1>Welcome, <span style={{color: "#1167b1"}}>{name}</span>!</h1>
+            <div className="interestsContainer">
                <p className="interestsTitle">MY INTERESTS</p>
                 <div className="interests">
-                    <h3 className="politicsInterests">POLITICS</h3>
-                    <h3 className="businessInterests">BUSINESS</h3>
-                    <h3 className="techInterests">TECH</h3>
-                    <h3 className="scienceInterests">SCIENCE</h3>
-                    <h3 className="sportsInterests">SPORTS</h3>
+                    <h3 onClick={handleClickPolitics} className={politics ? "politicsInterestsActive" : "politicsInterests"}>POLITICS</h3>
+                    <h3 onClick={handleClickBusiness} className={business ? "businessInterestsActive" : "businessInterests"}>BUSINESS</h3>
+                    <h3 onClick={handleClickTech} className={tech ? "techInterestsActive" : "techInterests"}>TECH</h3>
+                    <h3 onClick={handleClickScience} className={science ? "scienceInterestsActive" : "scienceInterests"}>SCIENCE</h3>
+                    <h3 onClick={handleClickSports} className={sports ? "sportsInterestsActive" : "sportsInterests"}>SPORTS</h3>
                 </div>
             </div>
             <button className="buttonSave"> SAVE </button>
